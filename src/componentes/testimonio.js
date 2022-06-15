6838a3617a539e1c1222337efa77ead5163d3476
EnriqueAguilarP <dialectikos@gmail.com>
2022-06-15 18:06:05 -0500

    Creando componente con imagen

---
+++ src/componentes/testimonio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function Testimonio(){
+  return(
+    <div className = 'contenedor-testimonio'>
+      <img 
+      className='imagen-testimonio' 
+      src={require('../imagenes/enchiladas.jpg')}
+      alt='Foto de comida'
+      />
+
+    </div>
+  );
+}
